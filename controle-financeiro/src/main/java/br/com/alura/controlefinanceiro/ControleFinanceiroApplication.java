74d1d5f91a19e192578e64d2ae11df533716d316
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2022-12-14 12:21:04 -0300

    Depedencia Postgresql

--- controle-financeiro/src/main/java/br/com/alura/controlefinanceiro/ControleFinanceiroApplication.java
+++ controle-financeiro/src/main/java/br/com/alura/controlefinanceiro/ControleFinanceiroApplication.java
@@ -2,9 +2,7 @@ package br.com.alura.controlefinanceiro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = { "br.com.alura.controlefinanceiro.repositories" })
 @SpringBootApplication
 public class ControleFinanceiroApplication {
 
