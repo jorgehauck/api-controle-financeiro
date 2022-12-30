ed196418b0ddb038d1f7b726190ad3f57fa77126
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2022-12-30 20:41:15 -0300

    Remoção de arquivo import.sql

--- project/src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO receitas(descricao, valor, data) VALUES ('Salario', 4570.00, '2022-12-07');
-INSERT INTO receitas(descricao, valor, data) VALUES ('Dividendos', 1570.00, '2022-10-15');
-
-INSERT INTO despesas(descricao, valor, data, categoria, receitas_id) VALUES('Restaurante', 250.0, '2022-12-07', 'ALIMENTACAO', 1);
-INSERT INTO despesas(descricao, valor, data, categoria, receitas_id) VALUES('Cinema', 57.90, '2022-10-17', 'LAZER', 2);
