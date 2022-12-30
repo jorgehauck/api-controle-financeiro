0e91a058e5873b13a9e7227d2999af00dff9fede
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2022-12-30 19:50:27 -0300

    Importação de dados

--- project/src/main/java/com/controle/financeiro/project/exceptionhandler/ControleFinanceiroAdvice.java
+++ project/src/main/java/com/controle/financeiro/project/exceptionhandler/ControleFinanceiroAdvice.java
@@ -4,11 +4,11 @@ import java.util.Date;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ControleFinanceiroAdvice {
 
 	@ResponseBody
