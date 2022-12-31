c4f4d76e65cc900cddcad14993bb97c1323eeb8f
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2022-12-31 14:38:24 -0300

    Update Receitas.java

--- project/src/main/java/com/controle/financeiro/project/model/Receitas.java
+++ project/src/main/java/com/controle/financeiro/project/model/Receitas.java
@@ -14,7 +14,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "receitas")
+@Table(name = "tb_receitas")
 public class Receitas implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
