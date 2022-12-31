32753620fd8a8329df77fa860e1ec206a5ab30c8
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2022-12-31 20:35:05 -0300

    Anotação adicionado ao método main para escanear classes de domínio

--- project/src/main/java/com/controle/financeiro/project/ProjectApplication.java
+++ project/src/main/java/com/controle/financeiro/project/ProjectApplication.java
@@ -2,7 +2,9 @@ package com.controle.financeiro.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = "com.controle.financeiro.project.model")
 @SpringBootApplication
 public class ProjectApplication {
 
